1b7e4247f9514c64812f6a1f675996cbfb485344
surry <surry@users.noreply.github.com>
2017-06-10 02:40:02 -0400

    Fleshed out support for retrieving tomorrow's weather. Added support for emoji forecasts.

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ const https = require('https');
 
 const request = require('request');
 
-var bot_token = process.env.SLACK_BOT_TOKEN || '';
+var botToken = process.env.SLACK_BOT_TOKEN || '';
 const darkskyToken = process.env.DARKSKY_TOKEN || '';
 
-if (!bot_token || !darkskyToken) {
+if (!botToken || !darkskyToken) {
     console.error('SLACK_BOT_TOKEN and DARKSKY_TOKEN must be passed as environment variables!');
     process.exit(1);
 }
 
 const latitude = 38.8892681;
 const longitude = -77.0501425;
-const darkskyURL = `https://api.darksky.net/forecast/${darkskyToken}/${latitude},${longitude}`;
 
-var rtm = new RtmClient(bot_token);
+// we don't need the minutely or hourly forecast, so we'll exclude them
+const darkskyURL = `https://api.darksky.net/forecast/${darkskyToken}/${latitude},${longitude}?exclude=minutely,hourly`;
+
+var rtm = new RtmClient(botToken);
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
@@ -28,39 +30,99 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 let channel;
 let rtmConnectionOpened = false;
 
+const icon2Emoji = {
+    'clear-day': '\u{2600}',
+    'clear-night': '\u{1f31d}',
+    'rain': '\u{1f327}',
+    'snow': '\u{1f328}',
+    'sleet': '\u{2744}\u{2614}',
+    'wind': '\u{1f32c}',
+    'fog': '\u{1f32b}',
+    'cloudy': '\u{1f325}',
+    'partly-cloudy-day': '\u{1f324}',
+    'partly-cloudy-night': '\u{2601}'
+};
+
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-    console.log('Message:', message);
 
     if (!channel) {
         channel = message.channel;
     }
 
+    // forgive trailing spaces and ignore case
     const normalizedMessage = message.text.trim().toLowerCase();
-    let now;
+    let now = false;
+    let tomorrow = false;
 
-    if (normalizedMessage === 'weather now') {
+    if (normalizedMessage.indexOf('weather now') !== -1) {
         now = true;
-    } else if (normalizedMessage === 'weather tomorrow') {
-        now = false;
     }
 
-    request(darkskyURL, (error, response, body) => {
-        console.error(error);
-        let json = JSON.parse(body);
-        if (json) {
-            if (rtmConnectionOpened) {
-                if (now === true && json.currently.summary) {
-                    let message = `The weather is currently ${json.currently.summary} and it feels like ${json.currently.apparentTemperature}\u00b0 F.`;
-                    rtm.sendMessage(message, channel);
-                } else if (now === false) {
-                    //rtm.sendMessage(, channel);
+    if (normalizedMessage.indexOf('weather tomorrow') !== -1) {
+        tomorrow = true;
+    }
+
+    if (now || tomorrow) {
+        request(darkskyURL, (error, response, body) => {
+            if (error) {
+                console.error(error);
+            } else {
+                let json = JSON.parse(body);
+
+                if (json) {
+                    if (rtmConnectionOpened) {
+                        if (now && json.currently.summary) {
+                            let summary = json.currently.summary.toLowerCase();
+                            let message;
+                            if (json.currently.icon) {
+                                message = icon2Emoji[json.currently.icon];
+                            }
+                            message += `The weather is currently ${summary}`
+                            if (!message.endsWith('.')) {
+                                message += '.';
+                            }
+                            if (json.currently.apparentTemperature) {
+                                message += `It feels like ${json.currently.apparentTemperature}\u00b0 F.`;
+                            }
+                            rtm.sendMessage(message, channel);
+                        }
+
+                        if (tomorrow) {
+                            if (json.daily.data.length >= 2) {
+                                let summary = json.daily.data[1].summary.toLowerCase();
+                                let message;
+                                if (json.currently.icon) {
+                                    message = icon2Emoji[json.currently.icon];
+                                }
+                                message += `The weather tomorrow will be ${summary}`
+                                if (!message.endsWith('.')) {
+                                    message += '.';
+                                }
+                                if (json.daily.data[1].apparentTemperature) {
+                                    message += `It will feel like ${json.daily.data[1].apparentTemperature}\u00b0 F.`;
+                                }
+                                rtm.sendMessage(message, channel);
+                            } else {
+                                rtm.sendMessage(`There is no forecast for tomorrow! The end is nigh.`, channel);
+                            }
+                        }
+                    }
                 }
             }
+        });
+    } else {
+        if (rtmConnectionOpened) {
+            if (normalizedMessage === 'help') {
+                rtm.sendMessage('try @surry-interview \'weather now\' or \'weather tomorrow\'');
+            } else {
+                rtm.sendMessage('I did not understand that command, try @surry-interview \'weather now\' or \'weather tomorrow\'');
+                console.log('Ignoring unknown command: ' + normalizedMessage.substring(0, 20) + '...');
+            }
         }
-    });
+    }
 });
 
-// you need to wait for the client to fully connect before you can send messages
+// need to wait for the client to fully connect before we can send messages
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
     rtmConnectionOpened = true;
 });
